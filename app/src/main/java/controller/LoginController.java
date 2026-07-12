3d57ed34890efdaa96e1ebb797494a856c08d536
amanda <a266881@dac.unicamp.br>
2026-07-12 16:36:19 -0300

    create UserNotFound Exception

--- app/src/main/java/controller/LoginController.java
+++ app/src/main/java/controller/LoginController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import exception.UserNotFoundException;
 import model.User;
 import model.UserProfileDTO;
 import service.Services;
@@ -12,7 +13,6 @@ public class LoginController {
     private final Runnable onSuccess;
     private final DataManager manager = Services.getManager();
 
-
     public LoginController(LoginScreen screen, Runnable onSuccess) {
         this.screen = screen;
         this.onSuccess = onSuccess;
@@ -23,28 +23,28 @@ public class LoginController {
     private void login() {
 
         if (screen.getEmail().isBlank() || screen.getPassword().isBlank()) {
-
             screen.showError("Todos os campos são obrigatórios.");
             return;
         }
 
-        UserProfileDTO info = manager.readData(screen.getEmail(), UserProfileDTO.class).get(0);
+        try {
+            UserProfileDTO info = manager.findUser(screen.getEmail());
 
-        if (info == null) {
-            screen.showError("Email inválido ou sistema sem cadastros");
-            return;
+            if (info.password().equals(screen.getPassword())) {
+
+                UserProfileDTO userInfo = manager.readData(screen.getEmail(), UserProfileDTO.class).get(0);
+
+                User currentUser = new User(userInfo);
+                Session.setLoggedUser(currentUser);
+                onSuccess.run();
+                
+            } else {
+                screen.showError("A senha está incorreta.");
+            }
+
         }
-
-        if (info.password().equals(screen.getPassword())) {
-
-            UserProfileDTO userInfo = manager.readData(screen.getEmail(), UserProfileDTO.class).get(0);
-
-            User currentUser = new User(userInfo);
-            Session.setLoggedUser(currentUser);
-            onSuccess.run();
-            return;
+        catch(UserNotFoundException e){
+            screen.showError(e.getMessage());
         }
-
-        screen.showError("Login não existe ou a senha está incorreta.");
     }
 }
